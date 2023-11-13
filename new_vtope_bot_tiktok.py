be9fb1ec136c97415fdfcab86d1f4291c2543ecc
Professional <ttkagentusolie@gmail.com>
2023-11-13 18:09:10 +0800

    Изменения от 13.11.32

--- new_vtope_bot_tiktok.py
+++ new_vtope_bot_tiktok.py
@@ -1,5 +1,7 @@
 from requests import Session
-import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 headers = {
     'Accept': '*/*',
@@ -18,62 +20,57 @@ headers = {
 }
 
 
-# category = input('Введите категорию: =>')
-# article = input('Введите артикул: =>')
-
-
 def main(base_url):
     count = 0
     s = Session()
     s.headers.update(headers)
     response = s.get(base_url)
     data = response.json()
-    # print(data)
-    print(data)
-    for item in data['data']['products']:
+
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.implicitly_wait(10)
+
+    for item in data['data']['products'][:15]:
         count += 1
         print('*' * 30)
         print("Порядковый №", count)
-        print("Заголовок =>", item['name'])
+        # print("Заголовок =>", item['name'])
         print("Бренд =>", item['brand'])
+        print("Продавец =>", item['supplier'])
         print("Артикул =>", item['id'])
-        print("Фотографии =>", item['pics'])
-        print("Цена до скидки =>", item['priceU'] / 100)
-        print("Цена =>", item['salePriceU'] / 100)
+        # print("Фотографии =>", item['pics'])
+        print("Цена до скидки =>", int(item['priceU'] / 100))
+        print("Цена =>", int(item['salePriceU'] / 100))
         print("Скидка =>", item['sale'], "%")
         print("Отзывы =>", item['feedbacks'])
         print("Рейтинг =>", item['rating'])
-        print("Рейтинг отзывов =>", item['reviewRating'])
+        # print("Рейтинг отзывов =>", item['reviewRating'])
+
+        # # Преобразование текущей даты и времени
+        # current_date = time.time()
+        # time1 = item['time1']
+        # time2 = item['time2']
+        # dist = item['dist']
+        # delivery_hours = ((time1 + time2) * 3600)
+        # data_time = (current_date + delivery_hours + 28800)
+        # print(int(data_time))
+
         try:
             print("Название акции =>", item['promoTextCat'])
-        except:
+        except KeyError:
             print('В акции не участвует')
 
-base_url = 'https://search.wb.ru/exactmatch/ru/common/v4/search?TestGroup=no_test&TestID=no_test&appType=1&curr=rub&dest=-455222&f4424=12868&fcolor=16119260&page=1&query=iphone%2012&resultset=catalog&sort=priceup&spp=25&suppressSpellcheck=false&uclusters=0'
+        driver.get(f"https://www.wildberries.ru/catalog/{item['id']}/detail.aspx?targetUrl=SP")
+        try:
+            delivery_element = driver.find_elements(By.CLASS_NAME, "delivery__title")[0].text
+            print("Дата доставки =>", delivery_element)
+        except IndexError:
+            print('Нет в наличии')
+
+    driver.quit()
+
+
+base_url = 'https://search.wb.ru/exactmatch/ru/common/v4/search?TestGroup=no_test&TestID=no_test&appType=1&curr=rub&dest=-445278&f4424=12868&fcolor=0&page=1&query=iphone%2013%20pro%20max&resultset=catalog&sort=priceup&spp=25&suppressSpellcheck=false'
 main(base_url)
-
-
-# import time
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-#
-# ids_list = [
-#     176754607,
-#     180518469,
-#     180518531,
-#     178281171,
-#     179567092,
-#     183523312,
-#     178297328,
-#     185696853,
-#     177819725,
-#     183821736,
-# ]
-# driver = webdriver.Chrome()
-# for i in ids_list:
-#     driver.get(f"https://www.wildberries.ru/catalog/{i}/detail.aspx?targetUrl=SP")  # Замените URL на нужный
-#     # time.sleep(3)
-#     driver.implicitly_wait(10)
-#     delivery_element = driver.find_elements(By.CLASS_NAME, "delivery__title")[0].text
-#     print(delivery_element)
